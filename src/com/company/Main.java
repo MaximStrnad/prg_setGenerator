ac20da73297a313cd07bb034242f6cf2ecfe6b09
Maxim Strnad <Maxim.Strnad@ssinfis.cz>
2018-10-24 12:19:51 +0200

    mordekaiser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,18 @@ public class Main {
 	// write your code here
         Scanner sc = new Scanner(System.in);
         int setLength = 0;
-
-
+        int[] arr_a = {1,2,3,4,5,6,7,8,9,10,12,34};
+        int[] arr_b = {2,4,6,5,8,10,33};
+        Set a = new Set(arr_a);
+        Set b = new Set(arr_b);
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+        b.sort();
+        System.out.println(b.toString());
+        System.out.println(Set.intersection(a,b));
+        System.out.println("intersection " + Set.intersection(a,b).toString());
+        System.out.println("diff " + Set.difference(a,b).toString());
+        System.out.println("supp " + Set.supplement(a,b).toString());
+        System.out.println("union " + Set.union(a,b).toString());
     }
 }
--- src/com/company/Set.java
+++ src/com/company/Set.java
@@ -37,10 +37,9 @@ public class Set
         return this.array.length;
     }
 
-    static String renderSet(Set set)
+    public String toString()
     {
-        String res = Arrays.toString(set.array);
-        System.out.println(res);
+        String res = Arrays.toString(this.array);
         res = res.replace("[","{");
         res = res.replace("]","}");
         return res;
@@ -51,16 +50,105 @@ public class Set
         int[] c = new int[a.length];
         int count = 0;
         for (int x = 0; x < a.length; x++ ) {
-            boolean isSet = false;
             for (int y = 0; y < b.length; y++ ) {
                 if (a[x] == b[y]) {
-                    isSet = true;
-                    break;
+                    c[count] = a[x];
+                    count++;
                 }
             }
+
+        }
+        int[] res = new int[count];
+        for (int x = 0; x < count; x ++){
+            res[x] = c[x];
+        }
+        return new Set(res);
+    }
+    static Set difference (Set s1, Set s2) {
+        int[] a = s1.array;
+        int[] b = s2.array;
+        int[] c = new int[a.length];
+        int count = 0;
+
+        for (int x = 0; x < a.length; x++ ) {
+            boolean isSet = false;
+            for(int z = 0; z < b.length; z++ ) {
+                if(a[x] == b[z]) {
+                    isSet = true;
+                }
+            }
+            if(!isSet) {
+                c[count] = a[x]; count++;
+
+            }
+        }
+        int[] res = new int[count];
+        for (int x = 0; x < count; x ++){
+            res[x] = c[x];
+        }
+        return new Set(res);
+    }
+    static Set supplement (Set s1, Set s2) {
+            return Set.difference(s1,s2);
+        }
+    public void sort () {
+        int[] nums = this.array;
+        for(int currentPlace = 0;currentPlace<nums.length-1;currentPlace++){
+            int smallest =  Integer.MAX_VALUE;
+            int smallestAt =  currentPlace+1;
+            for(int check =  currentPlace; check<nums.length;check++){
+                if(nums[check]<smallest){
+                    smallestAt  = check;
+                    smallest  = nums[check];
+                }
+            }
+            int temp =  nums[currentPlace];
+            nums[currentPlace]  = nums[smallestAt];
+            nums[smallestAt]  = temp;
+
+
+         }
+        this.array = nums;
+    }
+    static Set union (Set s1, Set s2) {
+        int[] a = s1.array;
+        int[] b = s2.array;
+        int[] c = new int[a.length + b.length];
+        int count = 0;
+        boolean isSet = false;
+
+        for (int x = 0; x < a.length; x ++){
             c[count] = a[x];
             count++;
         }
-        return new Set(c);
+        for (int x = 0; x < b.length; x ++){
+            c[count] = b[x];
+            count++;
+        }
+        count = 0;
+        int[] tmp = new int[c.length];
+        isSet = false;
+        for(int x = 0; x < c.length;x++) {
+            for(int y = x; y < c.length; y++){
+                if(c[x] == c[y] && x != y) {
+                    isSet = true;
+                    break;
+                } else {
+                    isSet = false;
+                }
+            }
+            if(!isSet) {
+                tmp[count] = c[x];
+                isSet = false;
+                count ++;
+            }
+        }
+        int[] res = new int[count];
+        for(int x = 0; x < count; x++) {
+            res[x] = tmp[x];
+        }
+        return new Set(res);
     }
+
+
 }
